5446f5847962db382a20a03ce05dac4874aa3115
duxmazter <46duxmazter@gmail.com>
2021-11-26 16:15:29 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int Num, evenNum = 0, oddNum = 0;
+
+    cout << "Enter an integer: " ;
+    cin >> Num;
+    while (Num != 0){
+        if (Num % 2 == 0){
+            evenNum++;
+        } else{
+            oddNum++;
+        }
+        cout << "Enter an integer: " ;
+        cin >> Num;
+    }
+
+    cout << "#Even numbers = " << evenNum << "\n";
+    cout << "#Odd numbers = " << oddNum;
+
     return 0;
 }
